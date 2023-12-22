2c59bcbbd1c0f3ed28e4758a4aa87153ed70bd8c
LLosTy <marcojurgenjuras@gmail.com>
2023-12-22 21:02:27 +0100

    Added mongoose and connected to DB, started working on list.js schema

---
+++ models/list.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+
+const listSchema = new mongoose.Schema({
+    listName :{
+
+    },
+    
+})
--- routes/lists.js
+++ routes/lists.js
@@ -3,20 +3,23 @@ const router = express.Router()
 const { projects } = require('../data')
 const { authUser } = require('../basicAuth')
 
+// Getting a list
 router.get('/' ,(req,res)=>{
   res.json('/lists')
   console.log("GET GET LISTS")
 })
 
-//POST = UPDATE
+// POST = CREATE a list
 router.post('/' ,(req,res)=>{
   res.json ('router.post/lists')
 })
 
-router.put('/' ,(req,res)=>{
+// Updating a list 
+router.patch('/' ,(req,res)=>{
   res.json('router.put/lists')
 })
 
+// Deleting a list
  router.delete('/:listId' ,(req,res)=>{
   res.json('router.delete/lists')
 })
